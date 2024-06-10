0c5b2cec3d7e830bf3d0dd8b0012956d8556f9f4
ZTZ <tzamirov@gmail.com>
2024-06-10 23:43:12 +0600

    setup layout

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,45 @@
+import '@/styles/global.css';
+
+import type {Metadata} from 'next';
+import {Inter as FontSans} from 'next/font/google';
+
+import {cn} from '@/utils/Helpers';
+import {AllLocales} from '@/utils/AppConfig';
+import {notFound} from 'next/navigation';
+import {NextIntlClientProvider, useMessages} from 'next-intl';
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
+export const metadata: Metadata = {
+  title: 'Pozitiv App',
+  description: 'Pozitiv App',
+};
+
+export default function RootLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: {locale: string};
+}>) {
+  if (!AllLocales.includes(params.locale)) notFound();
+
+  const messages = useMessages();
+
+  return (
+    <html lang={params.locale} suppressHydrationWarning>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
+        )}>
+        <NextIntlClientProvider locale={params.locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
