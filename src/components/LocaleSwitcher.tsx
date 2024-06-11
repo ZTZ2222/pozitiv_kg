65420e2924bdacceb8b52dc3f83d52103d318d93
ZTZ <tzamirov@gmail.com>
2024-06-11 12:11:48 +0600

    create locale switcher

---
+++ src/components/LocaleSwitcher.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useLocale } from "next-intl";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { usePathname, useRouter } from "@/libs/i18nNavigation";
+import { AppConfig } from "@/utils/AppConfig";
+
+export default function LocaleSwitcher() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const locale = useLocale();
+
+  const handleChange = (value: string) => {
+    router.push(pathname, { locale: value });
+    router.refresh();
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className="text-grey-3 space-x-2 p-2 focus-visible:ring-offset-0"
+          variant="ghost"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="size-6 stroke-current stroke-2"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            viewBox="0 0 24 24"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <path d="M3 12a9 9 0 1 0 18 0 9 9 0 0 0-18 0M3.6 9h16.8M3.6 15h16.8" />
+            <path d="M11.5 3a17 17 0 0 0 0 18M12.5 3a17 17 0 0 1 0 18" />
+          </svg>
+          <span>{locale.toLocaleUpperCase()}</span>
+          <svg
+            width="14"
+            height="9"
+            viewBox="0 0 14 9"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6.88993 5.42433L11.8397 0.474609L13.2539 1.88882L6.88993 8.25283L0.526028 1.88882L1.94023 0.474609L6.88993 5.42433Z"
+              fill="#979797"
+            />
+          </svg>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuRadioGroup value={locale} onValueChange={handleChange}>
+          {AppConfig.locales.map((elt) => (
+            <DropdownMenuRadioItem key={elt.id} value={elt.id}>
+              {elt.name}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
