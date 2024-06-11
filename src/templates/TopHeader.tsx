9acdc39893630e6e33924b5ec6299adbc9314221
ZTZ <tzamirov@gmail.com>
2024-06-12 00:48:19 +0600

    create TopHeader component

---
+++ src/templates/TopHeader.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+
+const TopHeader = () => {
+  return (
+    <header className="flex h-16 flex-col justify-between bg-gradient-to-r from-[#25CAD4] to-[#F809F3]">
+      <div className="relative h-4 w-full">
+        <Image
+          src="/assets/ornament-up.png"
+          alt="Ornaments"
+          fill
+          className="object-contain"
+        />
+      </div>
+      <div className="relative mx-4 h-4 w-[120px]">
+        <Image
+          src="/assets/logo/pozitiv.png"
+          alt="Logo"
+          fill
+          className="object-contain"
+        />
+      </div>
+      <div className="relative h-4 w-full self-end">
+        <Image
+          src="/assets/ornament-down.png"
+          alt="Ornaments"
+          fill
+          className="object-contain"
+        />
+      </div>
+    </header>
+  );
+};
+
+export default TopHeader;
