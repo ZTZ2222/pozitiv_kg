128fba2d793e1dce72c68380d9425032b425166d
ZTZ <tzamirov@gmail.com>
2024-06-12 00:23:29 +0600

    change Category Button Component from button to div

--- src/components/ButtonCategory.tsx
+++ src/components/ButtonCategory.tsx
@@ -1,25 +1,24 @@
 import { cn } from "@/utils/Helpers";
 import { Grip } from "lucide-react";
 import React from "react";
-import { Button } from "./ui/button";
 import { useTranslations } from "next-intl";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonCategoryProps {
   className?: string;
 }
 
-const ButtonCategory: React.FC<ButtonProps> = ({ className }) => {
+const ButtonCategory: React.FC<ButtonCategoryProps> = ({ className }) => {
   const t = useTranslations("Button");
   return (
-    <Button
+    <div
       className={cn(
-        "h-full gap-2.5 p-0 text-lg font-normal leading-tight text-grey-300",
+        "flex h-full gap-2.5 p-0 text-lg font-normal leading-tight text-grey-300",
         className,
       )}
     >
       <Grip className="h-6 w-6" />
       {t("categories")}
-    </Button>
+    </div>
   );
 };
 
