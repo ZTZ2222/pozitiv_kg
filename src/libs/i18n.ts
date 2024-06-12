8e20c60d97d6f1aff6d2e422f62dd28ba69b69ba
ZTZ <tzamirov@gmail.com>
2024-06-12 10:10:15 +0600

    update i18n with locales array imported from AppConfig

--- src/libs/i18n.ts
+++ src/libs/i18n.ts
@@ -1,12 +1,10 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
-
-// Can be imported from a shared config
-const locales = ["en", "ky", "ru", "uz"];
+import { AllLocales } from "@/utils/AppConfig";
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+  if (!AllLocales.includes(locale as any)) notFound();
 
   return {
     messages: (await import(`../locales/${locale}.json`)).default,
