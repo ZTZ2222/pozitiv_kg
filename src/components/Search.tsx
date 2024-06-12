98a19263b9a254158e159617d1b981c3ecdd5b32
ZTZ <tzamirov@gmail.com>
2024-06-12 12:27:21 +0600

    update search with media query rendering

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,23 +1,40 @@
+"use client";
+
 import React from "react";
 import { Search as SearchIcon } from "lucide-react";
 import { Input } from "./ui/input";
 import { useTranslations } from "next-intl";
+import { cn } from "@/utils/Helpers";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
-const Search = () => {
+interface SearchProps {
+  className?: string;
+}
+
+const Search: React.FC<SearchProps> = ({ className }) => {
   const t = useTranslations("Search");
+  const isSmallScreen = useMediaQuery("(max-width: 768px)");
   return (
-    <div className="text-grey-200 relative flex h-9 w-64 items-center rounded-[10px] border border-[#BCBCBC] bg-[#E4E4E4] md:h-14 md:w-[480px] md:border-none md:bg-white">
+    <div
+      className={cn(
+        "relative flex h-9 w-64 items-center rounded-[10px] border border-[#BCBCBC] bg-[#E4E4E4] text-gray-400 md:h-14 md:w-[480px] md:border-none md:bg-white",
+        className,
+      )}
+    >
       <SearchIcon className="ml-3 h-6 w-6 md:order-last md:ml-0 md:mr-7" />
-      <Input
-        type="search"
-        placeholder={t("placeholder_mobile")}
-        className="h-full border-none px-2 text-base font-light md:hidden"
-      />
-      <Input
-        type="search"
-        placeholder={t("placeholder_desktop")}
-        className="hidden h-full border-none pl-7 text-lg md:order-1 md:block"
-      />
+      {isSmallScreen ? (
+        <Input
+          type="search"
+          placeholder={t("placeholder_mobile")}
+          className="h-full border-none px-2 text-base font-light"
+        />
+      ) : (
+        <Input
+          type="search"
+          placeholder={t("placeholder_desktop")}
+          className="h-full border-none pl-7 text-lg md:order-1"
+        />
+      )}
     </div>
   );
 };
