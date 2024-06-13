901745adc8589f854728fe5ea800ad0b6faf6d29
ZTZ <tzamirov@gmail.com>
2024-06-13 14:00:01 +0600

    rename global.css to globals.css + update path in layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,21 +1,22 @@
-import '@/styles/global.css';
+import "@/styles/globals.css";
 
-import type {Metadata} from 'next';
-import {Inter as FontSans} from 'next/font/google';
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
 
-import {cn} from '@/utils/Helpers';
-import {AllLocales} from '@/utils/AppConfig';
-import {notFound} from 'next/navigation';
-import {NextIntlClientProvider, useMessages} from 'next-intl';
+import { cn } from "@/utils/Helpers";
+import { AllLocales } from "@/utils/AppConfig";
+import { notFound } from "next/navigation";
+import { NextIntlClientProvider, useMessages } from "next-intl";
+import Header from "@/templates/Header";
 
 const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-  title: 'Pozitiv App',
-  description: 'Pozitiv App',
+  title: "Pozitiv App",
+  description: "Pozitiv App",
 };
 
 export default function RootLayout({
@@ -23,7 +24,7 @@ export default function RootLayout({
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: {locale: string};
+  params: { locale: string };
 }>) {
   if (!AllLocales.includes(params.locale)) notFound();
 
@@ -33,10 +34,12 @@ export default function RootLayout({
     <html lang={params.locale} suppressHydrationWarning>
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased',
+          "min-h-screen bg-background font-sans antialiased",
           fontSans.variable,
-        )}>
+        )}
+      >
         <NextIntlClientProvider locale={params.locale} messages={messages}>
+          <Header />
           {children}
         </NextIntlClientProvider>
       </body>
