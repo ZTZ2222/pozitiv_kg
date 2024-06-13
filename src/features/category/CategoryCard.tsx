35e2720db882809a00a4082812d634aef1e0ec16
ZTZ <tzamirov@gmail.com>
2024-06-13 15:05:54 +0600

    update category card with variant button

--- src/features/category/CategoryCard.tsx
+++ src/features/category/CategoryCard.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/accordion";
 import { cn } from "@/utils/Helpers";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 interface CategoryCardProps {
   title: string;
@@ -17,6 +18,7 @@ interface CategoryCardProps {
     href: string;
   }[];
   className?: string;
+  variant?: "accordion" | "button";
 }
 
 const CategoryCard: React.FC<CategoryCardProps> = ({
@@ -24,7 +26,23 @@ const CategoryCard: React.FC<CategoryCardProps> = ({
   icon,
   subs,
   className,
+  variant = "accordion",
 }) => {
+  if (variant === "button") {
+    return (
+      <Button
+        className={cn(
+          "h-fit w-[224px] justify-start gap-5 rounded-[5px] border border-yellow-400 bg-indigo-400 p-5 px-5 py-2.5 text-white",
+          className,
+        )}
+      >
+        {icon && <Image src={icon} alt={title} width={50} height={50} />}
+        <span className="text-wrap text-start text-base font-medium">
+          {title}
+        </span>
+      </Button>
+    );
+  }
   return (
     <AccordionItem
       value={title}
@@ -41,7 +59,11 @@ const CategoryCard: React.FC<CategoryCardProps> = ({
       </AccordionTrigger>
       <AccordionContent className="flex flex-col px-3">
         {subs?.map((sub) => (
-          <Link href={sub.href} className="px-2 py-2.5 hover:bg-black/5">
+          <Link
+            key={sub.title}
+            href={sub.href}
+            className="px-2 py-2.5 hover:bg-black/25"
+          >
             {sub.title}
           </Link>
         ))}
