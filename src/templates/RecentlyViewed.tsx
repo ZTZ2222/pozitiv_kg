bd8f4b5c34b72eb59628d49aa9f401634956b133
ZTZ <tzamirov@gmail.com>
2024-06-14 00:40:40 +0600

    fix h2 with appropriate text in recenlty viewed comp

--- src/templates/RecentlyViewed.tsx
+++ src/templates/RecentlyViewed.tsx
@@ -9,7 +9,7 @@ const RecentlyViewed = ({ className }: { className?: string }) => {
   const t = useTranslations("Heading");
   return (
     <section className={cn("container", className)}>
-      <h2 className="text-lg font-semibold">{t("recommended_ads")}</h2>
+      <h2 className="text-lg font-semibold">{t("recently_viewed")}</h2>
       <div className="mt-[30px] grid grid-cols-2 gap-4">
         {ads_2.map((ad: Ad) => (
           <AdCard key={ad.id} {...ad} />
