664bab46ef650e25c566b6b73a70f41327a21af6
ZTZ <tzamirov@gmail.com>
2024-06-18 14:41:27 +0600

    create chunking array helper function

--- src/utils/Helpers.ts
+++ src/utils/Helpers.ts
@@ -1,7 +1,8 @@
-import {type ClassValue, clsx} from 'clsx';
-import {twMerge} from 'tailwind-merge';
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-import {AppConfig} from './AppConfig';
+import { AppConfig } from "./AppConfig";
+import { IAttribute } from "@/types/category.interface";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -14,3 +15,17 @@ export const getI18nPath = (url: string, locale: string) => {
 
   return `/${locale}${url}`;
 };
+
+export function chunkArray(arr: IAttribute[], size: number) {
+  return arr.reduce<IAttribute[][]>((chunks, item, index) => {
+    const chunkIndex = Math.floor(index / size);
+
+    if (!chunks[chunkIndex]) {
+      chunks[chunkIndex] = [];
+    }
+
+    chunks[chunkIndex].push(item);
+
+    return chunks;
+  }, []);
+}
