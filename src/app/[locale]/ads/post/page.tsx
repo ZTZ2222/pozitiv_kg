ff6d0ac24329603e13b739c3d156a1809ba1ab66
ZTZ <tzamirov@gmail.com>
2024-06-24 11:26:58 +0600

    rename "ad" route to "ads"

--- src/app/[locale]/ads/post/page.tsx
+++ src/app/[locale]/ads/post/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { category_attrs } from "@/utils/fake_api";
-import CreateAdForm from "@/features/ads/forms/CreateAdForm";
+import CreateAdForm from "@/components/ads/forms/CreateAdForm";
 
 const PostAd = async () => {
   const data = category_attrs;
