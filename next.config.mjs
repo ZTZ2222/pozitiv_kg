dc91f18423d9e73dedc54335f974a429797fd741
ZTZ <tzamirov@gmail.com>
2024-06-24 10:48:13 +0600

    add pozitiv to trusted domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin("./src/libs/i18n.ts");
+const withNextIntl = createNextIntlPlugin("./src/lib/i18n.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // add pozitiv.kg to domains
   images: {
-    domains: ["pozitiv.kg"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pozitiv.kg",
+      },
+    ],
   },
 };
 
