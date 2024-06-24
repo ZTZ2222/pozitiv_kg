fe790f7eb53a8536bd02f24893ed93ee2156f295
ZTZ <tzamirov@gmail.com>
2024-06-24 23:10:50 +0600

    connect chat to api

--- src/app/[locale]/chat/[chatId]/page.tsx
+++ src/app/[locale]/chat/[chatId]/page.tsx
@@ -1,6 +1,7 @@
-import { ChatLayout } from "@/features/chat/ChatLayout";
-import ChatTopBar from "@/features/chat/ChatTopBar";
-import { chat_list } from "@/utils/fake_api";
+import { getChatList, getMessagesByChatId } from "@/actions/chat-actions";
+import { getUserInfo } from "@/actions/user-actions";
+import ChatLayout from "@/components/chat/ChatLayout";
+import ChatTopBar from "@/components/chat/ChatTopBar";
 import React from "react";
 
 interface ChatProps {
@@ -10,7 +11,11 @@ interface ChatProps {
 }
 
 const Chat = async ({ params: { chatId } }: ChatProps) => {
-  const chat = chat_list.find((chat) => chat.chat_id === chatId);
+  const userInfo = await getUserInfo();
+  const chatList = await getChatList();
+  const chatMessages = await getMessagesByChatId(chatId);
+
+  const chat = chatList.find((chat) => chat.chat_id === chatId);
 
   if (!chat) {
     return <div>Chat not found</div>;
@@ -20,7 +25,11 @@ const Chat = async ({ params: { chatId } }: ChatProps) => {
     <>
       <ChatTopBar chat={chat} />
       <div className="z-10 w-full max-w-5xl rounded-lg border text-sm lg:flex">
-        <ChatLayout />
+        <ChatLayout
+          chatList={chatList}
+          chatMessages={chatMessages}
+          userInfo={userInfo}
+        />
       </div>
     </>
   );
--- src/app/[locale]/chat/page.tsx
+++ src/app/[locale]/chat/page.tsx
@@ -2,36 +2,13 @@ import { Card } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { chat_list } from "@/utils/fake_api";
-import { IChat } from "@/types/chat.interface";
-import BackButton from "@/components/BackButton";
+import { zChat } from "@/types/chat.schema";
+import BackButton from "@/components/navigation/BackButton";
 import BottomScroller from "@/components/BottomScroller";
-
-// const fetchChats = async () => {
-//   const res = await fetch("https://pozitiv.kg/api/v1/chats", {
-//     cache: "no-store",
-//     headers: {
-//       Authorizaion:
-//         "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI3IiwianRpIjoiNGE5M2FlNDZkYTc1Mjk1MDA3ZWZkZDExNDhjMTRjMjcwNmY4ODc5YmZiZTJjMGFkOTZkMjA4MDUzNzk1Mjg3ZmQ0YjlkNzg4OGY2OWUxNDUiLCJpYXQiOjE3MTg3Njk5MTYuNTc0NDc0LCJuYmYiOjE3MTg3Njk5MTYuNTc0NDc2LCJleHAiOjE3NTAzMDU5MTYuNTY2MTg0LCJzdWIiOiI0ODIiLCJzY29wZXMiOltdfQ.i__TPFnsMx68e9mUxfSjYZPwpTzj1HxwDqWzs5gt2V_sHAh66GpETwBZ2slRuAVxoHMInVJjsi9nSq62p238tOQoq_lf9NboC9F2V5Z_tDl4CatCHX7GcEHn--fWeXbpuCd-7iGuAh7XsRpLrDMxIbcg5i5aw1CgMUBHoiu-ArVfJegxxoCgS1jjsBDcHxpAsgAu_nzqZff3h331VcIf-HaPq-uM7nDnDJapuKLjcMvY5B8s1MOHuGrS65Zv3id8jiHCKevhgeDGVAV4lVJqKpGe2B7o1mN2aMxswm3Sr_QI86kukGVW6acFj_gCe6IkMK9iK4po-Z-XaYRdvPqCMPPIm9QFWPos0FfikLOF5psRb62vV2dLojZQibZkp7z-I5wGej862Esm6r9vQBV3PLutEXTGmbx_cDPWOtYrp8Jh59TEXyG-iQrrQBYAuXSj8o1JJvsmyPpb3bcXxFqOYsbiPwCZuQ_2S1OR7z7ycfs-nJaHU-o_S9Gf4iJZ-S6ADWFh3i5PcX-yQH9_EaL6UawDNKwAefG3sUeRMP2xArgcz68QggncF7Y4H-LPV5DA6sg6QPQupceOEMOa1xdCAZime2nTH_ammHsRMxU2k4nxzGkU9dNwp5pNqPzEW6tGIIxK18x2VqANrIkVOGue1PJ3LBOwLf68L5HTXoQjqQs",
-//       "Content-Type": "application/json",
-//       "User-Agent": "PostmanRuntime/7.29.0",
-//       "Accept-Encoding": "gzip, deflate, br",
-//       Connection: "keep-alive",
-//       Accept: "*/*",
-//       "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-//     },
-//   });
-
-//   console.log(res.status);
-
-//   return res.json();
-// };
+import { getChatList } from "@/actions/chat-actions";
 
 const ChatList = async () => {
-  // const chats = await fetchChats();
-  // console.log(chats);
-
-  // const chat_list: IChat[] = [];
+  const chatList = await getChatList();
 
   return (
     <>
@@ -40,8 +17,8 @@ const ChatList = async () => {
         <h1 className="mx-auto -translate-x-5">Чаты</h1>
       </div>
       <main className="mt-10 flex h-[calc(100vh-168px)] w-full flex-col gap-2.5">
-        {chat_list.length > 0 ? (
-          chat_list.map((chat: IChat) => (
+        {chatList.length > 0 ? (
+          chatList.map((chat: zChat) => (
             <Link href={`/chat/${chat.chat_id}`} key={chat.chat_id}>
               <Card className="container flex min-h-[78px] items-center justify-between gap-4 rounded-none border-0 border-y border-gray-200 bg-gray-50 pb-3 pt-1.5 transition-colors duration-500 hover:bg-gray-200">
                 <div className="relative size-10 shrink-0">
@@ -91,7 +68,7 @@ const ChatList = async () => {
             </p>
           </div>
         )}
-        <BottomScroller dependencies={[chat_list.length]} />
+        <BottomScroller dependencies={[chatList.length]} />
       </main>
     </>
   );
