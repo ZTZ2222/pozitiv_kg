e450de7b21055636d04c8d086ad4080243adbb0f
ZTZ <tzamirov@gmail.com>
2024-06-24 22:54:43 +0600

    create breadcrumbs component

---
+++ src/components/navigation/BreadCrumbs.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+// import { useTranslations } from "next-intl";
+import { usePathname } from "@/lib/i18nNavigation";
+import Link from "next/link";
+
+const BreadCrumbs = () => {
+  //   const t = useTranslations("Navigation");
+  const pathname = usePathname();
+
+  // Split the pathname into parts
+  const parts = pathname.split("/").filter((part) => part !== "");
+
+  // Create breadcrumb links from the parts
+  const breadcrumbLinks = parts.map((part, index) => ({
+    label: part.charAt(0).toUpperCase() + part.slice(1),
+    href: `/${parts.slice(0, index + 1).join("/")}`,
+  }));
+
+  return (
+    <Breadcrumb className="container mt-7 py-1">
+      <BreadcrumbList>
+        {breadcrumbLinks.map((link, index) => (
+          <React.Fragment key={index}>
+            {index > 0 && <BreadcrumbSeparator />}
+            <BreadcrumbItem>
+              {index === breadcrumbLinks.length - 1 ? (
+                <BreadcrumbPage>{link.label}</BreadcrumbPage>
+              ) : (
+                <BreadcrumbLink asChild>
+                  <Link href={link.href}>{link.label}</Link>
+                </BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
+          </React.Fragment>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default BreadCrumbs;
