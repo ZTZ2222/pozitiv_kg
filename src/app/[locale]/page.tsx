9b1fdcc4a6c09eec5c3d4719da6bd7fc25551202
ZTZ <tzamirov@gmail.com>
2024-06-26 21:56:12 +0600

    remove bottom searchNfilter from home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,6 +8,7 @@ import MainFilter from "@/components/filter/MainFilter";
 import AdList from "@/components/ads/AdList";
 import { getCategories } from "@/actions/category-actions";
 import { getBanners } from "@/actions/banner-actions";
+import { getAds } from "@/actions/ads-actions";
 
 export async function generateMetadata() {
   const locale = await getLocale();
@@ -31,14 +32,10 @@ export default async function Home() {
 
   const categories = await getCategories();
   const banners = await getBanners();
+  const promotions = await getAds();
 
   return (
     <main>
-      {/* Bottom header */}
-      <div className="container flex items-center justify-between gap-[18px] py-1.5 shadow-sm md:hidden">
-        <Search />
-        <MainFilter categories={categories} />
-      </div>
       {/* End of bottom header */}
       <Swiper images={banners} className="mt-[30px]" />
       <CategoryList categories={categories} />
@@ -50,13 +47,13 @@ export default async function Home() {
       </div>
       <section className="container mt-[50px]">
         <h2 className="text-lg font-semibold">{t("recommended_ads")}</h2>
-        <AdList />
+        <AdList ads={promotions} />
       </section>
 
       <Swiper images={banners} className="mt-[50px]" />
       <section className="container mb-[100px] mt-[35px]">
         <h2 className="text-lg font-semibold">{t("recently_viewed")}</h2>
-        <AdList />
+        <AdList ads={promotions} />
       </section>
     </main>
   );
