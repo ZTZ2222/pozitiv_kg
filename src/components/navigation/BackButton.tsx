2c4518ddc2d24ca44db778f9ce0996572c3e378f
ZTZ <tzamirov@gmail.com>
2024-06-29 00:41:30 +0600

    update back button with text next to button

--- src/components/navigation/BackButton.tsx
+++ src/components/navigation/BackButton.tsx
@@ -4,22 +4,35 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft } from "lucide-react";
 import { useStepper } from "@/hooks/useStepper";
-import { useRouter } from "@/lib/i18nNavigation";
+import { usePathname, useRouter } from "@/lib/i18nNavigation";
+import { useTranslations } from "next-intl";
+import { cn } from "@/lib/utils";
 
-const BackButton = ({ variant }: { variant: "stepper" | "router" }) => {
+type Props = {
+  variant: "stepper" | "router";
+  className?: string;
+};
+
+const BackButton: React.FC<Props> = ({ variant, className }) => {
   const { prevStep, isDisabledStep } = useStepper();
   const router = useRouter();
+  const pathname = usePathname();
+  const t = useTranslations("BackButton");
+  const path = pathname.split("/").filter(Boolean).join("-").toLowerCase();
 
   if (variant === "router") {
     return (
-      <Button
-        className="flex h-fit shrink-0 justify-start p-1"
-        variant="ghost"
-        size="icon"
-        onClick={() => router.back()}
-      >
-        <ChevronLeft className="h-6 w-6" />
-      </Button>
+      <div className={cn("flex items-center", className)}>
+        <Button
+          className="flex h-fit shrink-0 justify-start p-1"
+          variant="ghost"
+          size="icon"
+          onClick={() => router.back()}
+        >
+          <ChevronLeft className="h-6 w-6" />
+        </Button>
+        <span className="font-light text-gray-400">{t(path) || path}</span>
+      </div>
     );
   }
 
@@ -28,7 +41,7 @@ const BackButton = ({ variant }: { variant: "stepper" | "router" }) => {
       disabled={isDisabledStep}
       onClick={prevStep}
       variant="ghost"
-      className="h-fit w-fit py-5 pl-0 text-gray-800"
+      className={cn("h-fit w-fit py-5 pl-0 text-gray-800", className)}
     >
       <ChevronLeft />
     </Button>
--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -26,97 +26,97 @@ const Header = () => {
   const backbtnChatsText = ["/chat"];
   // const backbtnUserInfoCallbtnDots = [/^\/chat\/[^/]+$/];
 
-  if (isSmallScreen) {
-    return (
-      <header>
-        <div className="flex h-16 flex-col justify-between bg-gradient-to-r from-cyan-400 to-fuchsia-500">
-          <div className="relative h-4 w-full">
-            <Image
-              src="/assets/ornament-up.png"
-              alt="Ornaments"
-              fill
-              className="object-contain"
-              sizes="(max-width: 600px) 100vw, 50vw"
-            />
-          </div>
-          <div className="container flex items-center justify-between">
-            <Link href="/" className="relative h-4 w-[120px]">
-              <Image
-                src="/assets/logo/pozitiv.png"
-                alt="Logo"
-                fill
-                className="object-contain"
-              />
-            </Link>
-            <LocaleSwitcher className="h-fit p-0 text-white/80" />
-          </div>
-          <div className="relative h-4 w-full self-end">
-            <Image
-              src="/assets/ornament-down.png"
-              alt="Ornaments"
-              fill
-              className="object-contain"
-            />
-          </div>
-        </div>
-        <nav className="container">
-          {/* Search & Filter */}
-          {matchesRoute(pathname, searchFilterRoutes) && (
-            <div
-              className={cn(
-                "flex items-center justify-between gap-[18px] py-1.5 shadow-sm md:hidden",
-                // "hidden",
-              )}
-            >
-              <Search />
-              <MainFilter />
-            </div>
-          )}
+  if (!isSmallScreen) {
+    return <header>Desktop View</header>;
+  }
 
-          {/* Back Button Router */}
-          {/* {matchesRoute(pathname, backbtnRoutes) && (
+  return (
+    <header>
+      <div className="flex h-16 flex-col justify-between bg-gradient-to-r from-cyan-400 to-fuchsia-500">
+        <div className="relative h-4 w-full">
+          <Image
+            src="/assets/ornament-up.png"
+            alt="Ornaments"
+            fill
+            className="object-contain"
+            sizes="(max-width: 600px) 100vw, 50vw"
+          />
+        </div>
+        <div className="container flex items-center justify-between">
+          <Link href="/" className="relative h-4 w-[120px]">
+            <Image
+              src="/assets/logo/pozitiv.png"
+              alt="Logo"
+              fill
+              className="object-contain"
+            />
+          </Link>
+          <LocaleSwitcher className="h-fit p-0 text-white/80" />
+        </div>
+        <div className="relative h-4 w-full self-end">
+          <Image
+            src="/assets/ornament-down.png"
+            alt="Ornaments"
+            fill
+            className="object-contain"
+          />
+        </div>
+      </div>
+      <nav className="container">
+        {/* Search & Filter */}
+        {matchesRoute(pathname, searchFilterRoutes) && (
+          <div
+            className={cn(
+              "flex items-center justify-between gap-[18px] py-1.5 shadow-sm md:hidden",
+              // "hidden",
+            )}
+          >
+            <Search />
+            <MainFilter />
+          </div>
+        )}
+
+        {/* Back Button Router */}
+        {/* {matchesRoute(pathname, backbtnRoutes) && (
             <>
               <BackButton variant="router" />
               <div>Back Button Routes</div>
             </>
           )} */}
 
-          {/* Back Button, Share & Dots */}
-          {matchesRoute(pathname, backbtnShareDotsRoutes) && (
-            <div className="mt-[30px] flex justify-between">
-              <BackButton variant="router" />
-              <div className="flex items-center gap-2.5">
-                <Button
-                  variant="ghost"
-                  size="xs"
-                  className="h-fit gap-1.5 text-fuchsia-500"
-                >
-                  Поделиться <Share className="size-5" />
-                </Button>
+        {/* Back Button, Share & Dots */}
+        {matchesRoute(pathname, backbtnShareDotsRoutes) && (
+          <div className="mt-[30px] flex justify-between">
+            <BackButton variant="router" />
+            <div className="flex items-center gap-2.5">
+              <Button
+                variant="ghost"
+                size="xs"
+                className="h-fit gap-1.5 text-fuchsia-500"
+              >
+                Поделиться <Share className="size-5" />
+              </Button>
 
-                <DotsDropdownMenu />
-              </div>
+              <DotsDropdownMenu />
             </div>
-          )}
+          </div>
+        )}
 
-          {/* Back Button & "Chats" Text */}
-          {matchesRoute(pathname, backbtnChatsText) && (
-            <div className="container mt-4 flex items-center">
-              <BackButton variant="router" />
-              <h1 className="mx-auto -translate-x-5">Чаты</h1>
-            </div>
-          )}
+        {/* Back Button & "Chats" Text */}
+        {matchesRoute(pathname, backbtnChatsText) && (
+          <div className="container mt-4 flex items-center">
+            <BackButton variant="router" />
+            <h1 className="mx-auto -translate-x-5">Чаты</h1>
+          </div>
+        )}
 
-          {/* Back Button, UserInfo, Call Button & 3 Dots */}
-          {/* {matchesRoute(pathname, backbtnUserInfoCallbtnDots) && (
+        {/* Back Button, UserInfo, Call Button & 3 Dots */}
+        {/* {matchesRoute(pathname, backbtnUserInfoCallbtnDots) && (
             <div>Back Button, UserInfo, Call Button & 3 Dots</div>
           )} */}
-        </nav>
-      </header>
-    );
-  }
-
-  return <header>Desktop View</header>;
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
