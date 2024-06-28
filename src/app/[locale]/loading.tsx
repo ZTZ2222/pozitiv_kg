7f671476d7f76847aeed3865ca7c7d884828dc7a
ZTZ <tzamirov@gmail.com>
2024-06-28 15:04:36 +0600

    update loading comp with pulsing pozitiv logo

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex h-screen items-center justify-center">Loading...</div>
+    <div className="fixed left-0 top-0 z-50 grid h-screen w-screen place-content-center bg-gradient-to-r from-cyan-400 to-fuchsia-500">
+      <div className="relative h-[377px] w-[322px] animate-pulse">
+        <Image
+          src="/assets/logo/circle-logo-big.png"
+          alt="Pozitiv Logo"
+          className="object-contain"
+          fill
+          sizes="(max-width: 600px) 100vw, 50vw"
+        />
+      </div>
+    </div>
   );
 };
 
