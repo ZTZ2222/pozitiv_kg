6d0e40e0aa808223f14482515726d95ada0de065
ZTZ <tzamirov@gmail.com>
2024-06-28 11:26:52 +0600

    create save + delete search actions

--- src/actions/favorite-actions.ts
+++ src/actions/favorite-actions.ts
@@ -3,7 +3,8 @@
 import { FavoriteItemSchema } from "@/types/ad.schema";
 import { actionClient } from "./safe-action";
 import { cookies } from "next/headers";
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
+import { SearchCreateSchema, SearchDeleteSchema } from "@/types/other.schema";
 
 export const updateFavorites = actionClient
   .schema(FavoriteItemSchema)
@@ -39,3 +40,61 @@ export const updateFavorites = actionClient
       return data;
     },
   );
+
+export const saveSearch = actionClient
+  .schema(SearchCreateSchema)
+  .action(async ({ parsedInput: { title, result, type } }): Promise<string> => {
+    const access_token = cookies().get("access_token")?.value;
+
+    const endpoint = `${process.env.NEXT_PUBLIC_API_URL}/products/save-search`;
+
+    const res = await fetch(endpoint, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, result, type }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    revalidateTag("saved-searches");
+
+    const { message } = await res.json();
+
+    return message;
+  });
+
+export const deleteSearch = actionClient
+  .schema(SearchDeleteSchema)
+  .action(async ({ parsedInput: { id } }): Promise<string> => {
+    const access_token = cookies().get("access_token")?.value;
+
+    console.log("Search ID: ", id);
+
+    const endpoint = `${process.env.NEXT_PUBLIC_API_URL}/products/delete-search/${id}`;
+
+    const res = await fetch(endpoint, {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    console.log(res.status, res);
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    revalidateTag("saved-searches");
+
+    const { message } = await res.json();
+
+    return message;
+  });
