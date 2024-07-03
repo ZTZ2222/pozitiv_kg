145d7bb4675ee050ff6b79b788635fa05a1e5fc6
ZTZ <tzamirov@gmail.com>
2024-07-03 11:25:23 +0600

    update login button with functionality and hover color

--- src/components/navigation/LoginButton.tsx
+++ src/components/navigation/LoginButton.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+"use client";
 
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
 import { UserCircle } from "@/components/icons";
+import { useRouter } from "@/lib/i18nNavigation";
 
 const LoginButton = () => {
   const t = useTranslations("Button");
+  const router = useRouter();
   return (
     <Button
       variant="ghost"
-      className="h-fit gap-3 px-2 py-1 text-base font-normal text-fuchsia-500"
+      className="h-fit gap-3 fill-fuchsia-500 px-2 py-1 text-base font-normal text-fuchsia-500 hover:fill-accent-foreground"
+      onClick={() => router.push("/login")}
     >
       <span>{t("login")}</span>
-      <UserCircle className="h-[30px] w-[30px] fill-fuchsia-500" />
+      <UserCircle className="h-[30px] w-[30px]" />
     </Button>
   );
 };
