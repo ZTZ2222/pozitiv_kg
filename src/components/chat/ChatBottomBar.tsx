3209bbcf8b2734e2ce8230e0ebdbd248a7727d44
ZTZ <tzamirov@gmail.com>
2024-07-07 00:46:09 +0600

    update chat bottom bar with form + actions + fix button group

--- src/components/chat/ChatBottomBar.tsx
+++ src/components/chat/ChatBottomBar.tsx
@@ -1,113 +1,141 @@
-import { FileImage, Paperclip, SendHorizontal } from "lucide-react";
-import React, { useRef, useState } from "react";
+"use client";
+
+import { SendHorizontal } from "lucide-react";
+import React, { useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
-import { zMessage } from "@/types/chat.schema";
+import { MessageCreateSchema, zMessageCreate } from "@/types/chat.schema";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormMessage,
+} from "@/components/ui/form";
+import { useAction } from "next-safe-action/hooks";
+import { sendMessage } from "@/actions/chat-actions";
 
-interface ChatBottombarProps {
-  sendMessage: (newMessage: zMessage) => void;
-}
+type Props = {
+  chatId: string;
+  className?: string;
+};
 
-export const BottombarIcons = [{ icon: FileImage }, { icon: Paperclip }];
+const ChatBottombar: React.FC<Props> = ({ chatId, className }) => {
+  const form = useForm<zMessageCreate>({
+    resolver: zodResolver(MessageCreateSchema),
+    defaultValues: {
+      chat_id: chatId,
+      message: "",
+    },
+  });
 
-export default function ChatBottombar({ sendMessage }: ChatBottombarProps) {
   const buttonGroupText = [
-    "Еще актуально?",
-    "Готов купить!",
-    "Заинтересован!",
-    "Заинтересован!",
+    "За сколько отдадите? ",
+    "Еще актуально? ",
+    "Интересует обмен? ",
+    "Торг возможен? ",
+    "Хочу купить! ",
+    "Я заинтересован! ",
   ];
-  const [message, setMessage] = useState("");
+
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setMessage(event.target.value);
-  };
+  const { execute, isExecuting } = useAction(sendMessage);
 
-  const handleSend = () => {
-    if (message.trim()) {
-      const newMessage: zMessage = {
-        id: 5555,
-        sender_id: {
-          id: 482,
-          name: "Mohamed Jalal Jalal",
-          image: "/assets/chat/man.png",
-        },
-        receiver_id: {
-          id: 471,
-          name: "Раушан Бай",
-          image: "https://pozitiv.kg/users/images/2023-11-01-65422edf287d1.png",
-        },
-        image: "",
-        created_at: new Date().toISOString(),
-        message: message.trim(),
-      };
-      sendMessage(newMessage);
-      setMessage("");
+  const onSubmit = (data: zMessageCreate) => {
+    execute(data);
 
-      if (inputRef.current) {
-        inputRef.current.focus();
-      }
+    form.reset();
+
+    if (inputRef.current) {
+      inputRef.current.focus();
     }
   };
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault();
-      handleSend();
+      if (!isExecuting) {
+        form.handleSubmit(onSubmit)();
+      }
     }
 
     if (event.key === "Enter" && event.shiftKey) {
       event.preventDefault();
-      setMessage((prev) => prev + "\n");
+      const currentValue = form.getValues("message");
+      form.setValue("message", currentValue + "\n");
     }
   };
 
+  const handleAppendText = (text: string) => {
+    const currentValue = form.getValues("message");
+    form.setValue("message", currentValue + text);
+  };
+
   return (
-    <div
-      className={cn(
-        "bg-white",
-        // "fixed bottom-0 left-0 right-0 z-50",
-      )}
-    >
-      <ScrollArea className="whitespace-nowrap border-t border-gray-400">
-        <div className="container flex gap-2.5 py-4">
-          {buttonGroupText.map((text, index) => (
-            <Button
-              onClick={() => setMessage((prev) => prev + text)}
-              key={index}
-              className="h-full bg-red-600 px-4 py-1 font-normal text-white hover:bg-red-800/70"
-            >
-              {text}
-            </Button>
-          ))}
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn("bg-white", className)}
+      >
+        {/* Button group */}
+        <ScrollArea className="whitespace-nowrap border-t border-gray-400">
+          <div className="flex w-max space-x-2.5 p-4">
+            {buttonGroupText.map((text, index) => (
+              <Button
+                type="button"
+                onClick={() => handleAppendText(text)}
+                key={index}
+                className="h-full bg-red-600 px-4 py-1 font-normal text-white hover:bg-red-800/70"
+              >
+                {text}
+              </Button>
+            ))}
+          </div>
+          <ScrollBar orientation="horizontal" className="opacity-0" />
+        </ScrollArea>
+
+        <Separator className="mx-auto w-[90%]" />
+
+        {/* Textarea and submit button */}
+        <div className="container flex w-full items-center justify-between gap-3.5 pb-7 pt-4">
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <>
+                <FormControl>
+                  <Textarea
+                    placeholder="Сообщение..."
+                    onKeyDown={handleKeyPress}
+                    autoComplete="off"
+                    className="flex h-14 w-full resize-none items-center overflow-hidden rounded-[10px] border bg-background bg-gray-200 focus-visible:ring-offset-0"
+                    {...field}
+                    ref={inputRef}
+                  />
+                </FormControl>
+                <FormMessage className="absolute bottom-1 left-5" />
+              </>
+            )}
+          />
+
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-9 w-9 shrink-0 bg-gray-400 hover:bg-gray-500"
+            type="submit"
+            disabled={isExecuting}
+          >
+            <SendHorizontal size={20} className="text-white" />
+          </Button>
         </div>
-        <ScrollBar orientation="horizontal" className="opacity-0" />
-      </ScrollArea>
-      <Separator className="mx-auto w-[90%]" />
-      <div className="container flex w-full items-center justify-between gap-3.5 pb-7 pt-4">
-        <Textarea
-          autoComplete="off"
-          value={message}
-          ref={inputRef}
-          onKeyDown={handleKeyPress}
-          onChange={handleInputChange}
-          name="message"
-          placeholder="Сообщение..."
-          className="flex h-9 w-full resize-none items-center overflow-hidden rounded-[10px] border bg-background bg-gray-200"
-        ></Textarea>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="h-9 w-9 shrink-0 bg-gray-400 hover:bg-gray-500"
-          onClick={handleSend}
-        >
-          <SendHorizontal size={20} className="text-white" />
-        </Button>
-      </div>
-    </div>
+      </form>
+    </Form>
   );
-}
+};
+
+export default ChatBottombar;
