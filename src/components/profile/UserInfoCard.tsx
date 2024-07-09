04493d42aa886264bed6726c03fa7f7b5b1c755f
ZTZ <tzamirov@gmail.com>
2024-07-09 14:17:59 +0600

    add link to user info card that redirects to seller profile

--- src/components/profile/UserInfoCard.tsx
+++ src/components/profile/UserInfoCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { cn, getInitials, getRandomColor } from "@/lib/utils";
 import { zSellerRead } from "@/types/user.schema";
 import React from "react";
@@ -7,10 +9,12 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "../ui/card";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Phone } from "lucide-react";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 type Props = {
   seller: zSellerRead;
@@ -18,32 +22,36 @@ type Props = {
 };
 
 const UserInfoCard: React.FC<Props> = ({ seller, className }) => {
+  const t = useTranslations("ContactBlock");
+
   const initials = getInitials(seller.name);
   const bgColor = getRandomColor();
   return (
-    <Card
-      className={cn("inline-flex gap-5 border-none md:border-solid", className)}
-    >
-      <CardHeader className="justify-center p-3">
-        <Avatar className="h-16 w-16">
-          <AvatarImage src={seller.image} className="object-cover" />
-          <AvatarFallback className={cn("text-xl font-medium", bgColor)}>
-            {initials}
-          </AvatarFallback>
-        </Avatar>
+    <Card className={cn("inline-flex border-none md:border-solid", className)}>
+      <CardHeader className="justify-center px-5">
+        <Link href={`/profile/${seller.id}`} className="hover:opacity-80">
+          <Avatar className="h-16 w-16">
+            <AvatarImage src={seller.image} className="object-cover" />
+            <AvatarFallback className={cn("text-xl font-medium", bgColor)}>
+              {initials}
+            </AvatarFallback>
+          </Avatar>
+        </Link>
       </CardHeader>
       <CardContent className="space-y-3.5 p-5">
-        <CardTitle className="text-lg font-normal">{seller.name}</CardTitle>
+        <Link href={`/profile/${seller.id}`} className="hover:underline">
+          <CardTitle className="text-lg font-normal">{seller.name}</CardTitle>
+        </Link>
         <CardDescription className="flex gap-2.5">
           <Phone className="size-[18px] fill-cyan-400 text-cyan-400" />
-          {seller.phone || "Телфон не указан"}
+          {seller.phone || "Телeфон не указан"}
         </CardDescription>
         <Button
           variant="outline"
-          className="h-fit px-2 py-1 text-cyan-400"
+          className="h-fit min-w-40 px-2 py-1 text-cyan-400"
           disabled={!seller.phone}
         >
-          Позвонить
+          {t("call")}
         </Button>
       </CardContent>
     </Card>
