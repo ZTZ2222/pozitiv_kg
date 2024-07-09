5b1da02ea9f1d5e7a9539d38c6894ffcda5755be
ZTZ <tzamirov@gmail.com>
2024-07-09 10:14:57 +0600

    create createPromotion action + update getMyAds

--- src/actions/ads-actions.ts
+++ src/actions/ads-actions.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import { zPromotionRead } from "@/types/ad.schema";
+import { zCityRead } from "@/types/other.schema";
 import { getLocale } from "next-intl/server";
+import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 
 export const getAds = async (
@@ -82,12 +84,14 @@ export const getRelatedAds = async (id: string): Promise<zPromotionRead[]> => {
   return data.items;
 };
 
-export const getMyAds = async (): Promise<zPromotionRead[]> => {
+type AdStatus = "active" | "pending" | "cancelled";
+
+export const getMyAds = async (status: AdStatus): Promise<zPromotionRead[]> => {
   const access_token = cookies().get("access_token")?.value;
   const locale = await getLocale();
 
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/products/my-list`,
+    `${process.env.NEXT_PUBLIC_API_URL}/products/my-list?status=${status}`,
     {
       cache: "no-store",
       credentials: "include",
@@ -98,6 +102,54 @@ export const getMyAds = async (): Promise<zPromotionRead[]> => {
       next: { tags: ["my-list"] },
     },
   );
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const { data } = await response.json();
+
+  return data.items;
+};
+
+export const createPromotion = async (data: FormData): Promise<string> => {
+  const access_token = cookies().get("access_token")?.value;
+
+  // data.forEach((value, key) => {
+  //   console.log({ value, key });
+  // });
+
+  const endpoint = `${process.env.NEXT_PUBLIC_API_URL}/products/add`;
+
+  const res = await fetch(endpoint, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+    body: data,
+  });
+
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status} - ${await res.json()}`);
+    // const error = await res.json();
+    // console.log(error);
+  }
+
+  revalidatePath("/profile");
+
+  const { message } = await res.json();
+
+  return message;
+};
+
+export const getCities = async (): Promise<zCityRead[]> => {
+  const locale = await getLocale();
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/cities`, {
+    cache: "no-store",
+    headers: {
+      "Accept-Language": locale,
+    },
+  });
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
