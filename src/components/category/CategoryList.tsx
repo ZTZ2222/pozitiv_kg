53e97b0f292bdc3b401ead6d4520ca26236a31e1
ZTZ <tzamirov@gmail.com>
2024-07-12 10:19:39 +0600

    replace media query with conditional css

--- src/components/category/CategoryList.tsx
+++ src/components/category/CategoryList.tsx
@@ -1,32 +1,25 @@
-"use client";
-
 import React from "react";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { zCategoryRead } from "@/types/category.schema";
 import CategoryButton from "./CategoryButton";
-import useMediaQuery from "@/hooks/useMediaQuery";
 
 const CategoryList = ({ categories }: { categories: zCategoryRead[] }) => {
-  const isTablet = useMediaQuery("(min-width: 768px)");
-
-  if (isTablet) {
-    return (
-      <div className="container my-[100px] grid grid-cols-3 gap-4 p-4 lg:grid-cols-4 xl:grid-cols-5">
-        {categories.map((category) => (
-          <CategoryButton key={category.id} {...category} />
-        ))}
-      </div>
-    );
-  }
   return (
-    <ScrollArea className="ml-4 mt-10 pb-4">
-      <div className="mr-4 flex gap-4">
+    <>
+      <ScrollArea className="ml-4 mt-10 pb-4 md:hidden">
+        <div className="mr-4 flex gap-4">
+          {categories.map((category) => (
+            <CategoryButton key={category.id} {...category} />
+          ))}
+        </div>
+        <ScrollBar className="hidden h-2.5 md:flex" orientation="horizontal" />
+      </ScrollArea>
+      <div className="container my-[100px] hidden grid-cols-3 gap-4 p-4 md:grid lg:grid-cols-4 xl:grid-cols-5">
         {categories.map((category) => (
           <CategoryButton key={category.id} {...category} />
         ))}
       </div>
-      <ScrollBar className="hidden h-2.5 md:flex" orientation="horizontal" />
-    </ScrollArea>
+    </>
   );
 };
 
