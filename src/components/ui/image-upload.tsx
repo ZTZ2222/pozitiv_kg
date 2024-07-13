024b59d521f1b3e34b2f55fd058b91b96370acae
ZTZ <tzamirov@gmail.com>
2024-07-13 23:01:36 +0600

    update image upload with viewing and deleting existing promo images

--- src/components/ui/image-upload.tsx
+++ src/components/ui/image-upload.tsx
@@ -9,14 +9,31 @@ import type { ControllerRenderProps } from "react-hook-form";
 import { Button } from "./button";
 import { motion, AnimatePresence } from "framer-motion";
 import { useTranslations } from "next-intl";
+import { zGalleryImage } from "@/types/ad.schema";
+import { useOptimisticAction } from "next-safe-action/hooks";
+import { deletePromotionImage } from "@/actions/ads-actions";
+import { toast } from "./use-toast";
 
 type Props = {
   field: ControllerRenderProps<any, any>;
+  existingImages?: zGalleryImage[];
 };
 
-const ImageUpload: React.FC<Props> = ({ field }) => {
+const ImageUpload: React.FC<Props> = ({ field, existingImages }) => {
   const t = useTranslations("PromotionForm");
   const [images, setImages] = useState<File[]>([]);
+
+  const { execute: deleteImage, optimisticState } = useOptimisticAction(
+    deletePromotionImage,
+    {
+      currentState: { existingImages },
+      updateFn: (state, item) => {
+        return {
+          existingImages: state.existingImages?.filter((i) => i.id !== item.id),
+        };
+      },
+    },
+  );
 
   const handleImageUpload = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
@@ -34,12 +51,52 @@ const ImageUpload: React.FC<Props> = ({ field }) => {
     field.onChange(newImages);
   };
 
+  const handleDeleteExistingImage = (image: zGalleryImage) => {
+    deleteImage(image);
+    toast({
+      description: t("delete-image-success-toast"),
+      duration: 3000,
+    });
+  };
+
   return (
     <FormItem>
       <FormLabel className="text-lg font-medium">{t("images-label")}</FormLabel>
       <FormControl>
         <div className="flex flex-wrap gap-5">
           <AnimatePresence>
+            {optimisticState.existingImages?.map((item, index) => (
+              <motion.div
+                key={index}
+                initial={{ y: "-20%", opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: "-20%", opacity: 0 }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+                className="relative size-24 overflow-hidden rounded-sm bg-gray-400"
+              >
+                <Image
+                  src={item.image}
+                  alt={`Uploaded Image ${index + 1}`}
+                  className="object-contain"
+                  fill
+                  sizes="(max-width: 600px) 100vw, 50vw"
+                />
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  type="button"
+                  onClick={handleDeleteExistingImage.bind(null, item)}
+                  className="absolute right-1 top-1 size-fit bg-red-500 p-0.5 text-white"
+                >
+                  <X className="size-5" />
+                </Button>
+                {index === 0 && (
+                  <span className="absolute bottom-2 left-1/2 -translate-x-1/2 text-nowrap rounded-sm bg-cyan-400 px-1 text-[10px] text-white">
+                    {t("main-image-label")}
+                  </span>
+                )}
+              </motion.div>
+            ))}
             {images.map((image, index) => (
               <motion.div
                 key={index}
@@ -65,7 +122,7 @@ const ImageUpload: React.FC<Props> = ({ field }) => {
                 >
                   <X className="size-5" />
                 </Button>
-                {index === 0 && (
+                {!optimisticState.existingImages && index === 0 && (
                   <span className="absolute bottom-2 left-1/2 -translate-x-1/2 text-nowrap rounded-sm bg-cyan-400 px-1 text-[10px] text-white">
                     {t("main-image-label")}
                   </span>
@@ -80,7 +137,6 @@ const ImageUpload: React.FC<Props> = ({ field }) => {
                 onChange={handleImageUpload}
                 multiple
                 accept="image/*"
-                required
               />
             </div>
           </AnimatePresence>
