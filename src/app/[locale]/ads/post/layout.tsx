282770d0d1908eab59b873edd9fe546c79136e59
ZTZ <tzamirov@gmail.com>
2024-07-21 23:46:22 +0600

    add back button to ad post

--- src/app/[locale]/ads/post/layout.tsx
+++ src/app/[locale]/ads/post/layout.tsx
@@ -1,4 +1,5 @@
 import { isAuthenticated } from "@/actions/user-actions";
+import BackButton from "@/components/navigation/BackButton";
 import BreadCrumbs from "@/components/navigation/BreadCrumbs";
 import { redirect } from "@/lib/i18nNavigation";
 import React from "react";
@@ -12,6 +13,11 @@ const AdPostLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <BreadCrumbs path={["post"]} className="container mb-[20px]" />
+      <BackButton
+        variant="router"
+        location="post"
+        className="container mt-[30px]"
+      />
       {children}
     </>
   );
